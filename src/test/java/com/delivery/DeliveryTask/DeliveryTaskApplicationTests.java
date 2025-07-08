c51e1e34887e4cf1cdeed9596fbbd3dd38d55d59
Abbass Hazimeh <abbasshazimeh700@gmail.com>
2025-07-08 04:13:57 +0300

    Add initial Spring Boot project

---
+++ src/test/java/com/delivery/DeliveryTask/DeliveryTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.DeliveryTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
